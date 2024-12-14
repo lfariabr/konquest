cf76e1ad0060ce0585a7431f502d00ce9f5457cb
Luis Faria <lfariabr@gmail.com>
2024-12-14 19:35:28 -0300

    Starting up Appointment implementation

--- messageShooter/utils/is_appointment_es.py
+++ messageShooter/utils/is_appointment_es.py
@@ -1,7 +1,21 @@
-# procedures_procorpo = ['AVALIAÇÃO ESTÉTICA',
-# 'AVALIAÇÃO INJETÁVEIS E INVASIVOS' ]
+# Rules to define what is an appointment for aesthetic services = Pró-Corpo
 
-# stores_to_remove = ['PRAIA GRANDE']
+# Appointment info
+procedures_es = ['AVALIAÇÃO ESTÉTICA', 'AVALIAÇÃO INJETÁVEIS E INVASIVOS']
+desired_status_es = ['Confirmado', 'Agendado']
 
-# procedure = [procedures_cirurgia]
-# store = "PLÁSTICA"
+# Stores:
+stores_exclude_es = ['PRAIA GRANDE', 'HOMA', 'PLÁSTICA']
+stores_include_es = [] # all, except stores_exclude
+
+# Useful intervals:
+intervals_es = {
+    '-1': 'd1',
+    '-2': 'd2',
+    '-3': 'd3',
+    '-4': 'd4',
+    '-5': 'd5',
+    '1': 'dMaisum'
+}
+
+interval_time = input - today()
